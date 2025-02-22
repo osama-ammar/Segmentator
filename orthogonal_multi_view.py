676272c70cc16271c72d8b724f5bf4b58789f731
Osama Ammar <54119005+osama-ammar@users.noreply.github.com>
2025-02-22 06:22:26 +0200

    add callback

--- orthogonal_multi_view.py
+++ orthogonal_multi_view.py
@@ -1,19 +1,16 @@
-from dash import Dash, html
+import json
 import imageio
 from dash_slicer import VolumeSlicer
-
+from dash import Dash, html, Input, Output, callback
 
 app = Dash(__name__, update_title=None)
 
 vol = imageio.volread("imageio:stent.npz")
-
-
-
 slicer0 = VolumeSlicer(app, vol, axis=0)
 slicer1 = VolumeSlicer(app, vol, axis=1)
 slicer2 = VolumeSlicer(app, vol, axis=2)
 
-slicer0.graph.config["scrollZoom"] = False
+slicer0.graph.config["scrollZoom"] = True
 slicer1.graph.config["scrollZoom"] = False
 slicer2.graph.config["scrollZoom"] = False
 
@@ -25,9 +22,20 @@ app.layout = html.Div(
     children=[
         html.Div([slicer0.graph, html.Br(), slicer0.slider, *slicer0.stores]),
         html.Div([slicer1.graph, html.Br(), slicer1.slider, *slicer1.stores]),
-        html.Div([slicer2.graph, html.Br(), slicer2.slider, *slicer2.stores]),
+        html.Div([slicer2.graph, html.Br(), slicer2.slider,html.Div(id='info'), *slicer2.stores]),
     ],
+    
 )
+
+
+@callback(
+    Output("info", "children"),
+    Input(slicer2.state.id, "data")
+)
+def update_info(state):
+    return json.dumps(state, indent=4)
+
+
 
 
 if __name__ == "__main__":
